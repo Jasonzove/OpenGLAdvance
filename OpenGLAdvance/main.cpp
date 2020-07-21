a2e53cba1c46ac6511568f29ee7035ff823f6403
kdvictor <huanliu_uestc@163.com>
2020-07-21 08:56:04 +0800

    第一个shader三角形

--- OpenGLAdvance/main.cpp
+++ OpenGLAdvance/main.cpp
@@ -1,10 +1,19 @@
 #include <windows.h>
 #include "glew.h"
+#include "Glm/ext.hpp"
+#include "Glm/glm.hpp"
 
 #include "utils.h"
 
+
 //#pragma  comment(lib, "opengl32.lib")
 #pragma  comment(lib, "glew32.lib")
+
+typedef struct VertexData
+{
+	float vertex[3];
+	float color[4];
+};
 
 LRESULT CALLBACK GLWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -55,11 +64,56 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	glewInit(); //glew初始化，必须放在wglMakeCurrent之后
 
 	GLuint program = CreateGPUProgram("./Res/shader/first_trangle.vs", "./Res/shader/first_trangle.fs");
+	GLuint posLocation, colorLocation, MLocation, VLocation, PLocation;
+	posLocation = glGetAttribLocation(program, "pos");
+	colorLocation = glGetAttribLocation(program, "color");
+	MLocation = glGetUniformLocation(program, "M");
+	VLocation = glGetUniformLocation(program, "V");
+	PLocation = glGetUniformLocation(program, "P");
+
+	VertexData vertexData[3];
+	vertexData[0].vertex[0] = 0.0;
+	vertexData[0].vertex[1] = 0.0;
+	vertexData[0].vertex[2] = -100.0;
+	vertexData[0].color[0] = 1.0;
+	vertexData[0].color[1] = 1.0;
+	vertexData[0].color[2] = 1.0;
+	vertexData[0].color[3] = 1.0;
+
+	vertexData[1].vertex[0] = 10.0;
+	vertexData[1].vertex[1] = 0.0;
+	vertexData[1].vertex[2] = -100.0;
+	vertexData[1].color[0] = 1.0;
+	vertexData[1].color[1] = 1.0;
+	vertexData[1].color[2] = 1.0;
+	vertexData[1].color[3] = 1.0;
+
+	vertexData[2].vertex[0] = 0.0;
+	vertexData[2].vertex[1] = 10.0;
+	vertexData[2].vertex[2] = -100.0;
+	vertexData[2].color[0] = 1.0;
+	vertexData[2].color[1] = 1.0;
+	vertexData[2].color[2] = 1.0;
+	vertexData[2].color[3] = 1.0;
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData)* 3, vertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glClearColor(41.0f / 255.0f, 71.0f / 255.0f, 121.0f / 255.0f, 1.0f);
 
 	ShowWindow(hwnd, SW_SHOW);
 	UpdateWindow(hwnd);
+
+	glm::mat4 projectionMat = glm::perspective(45.0f, 800.0f / 600.0f, 0.1f, 1000.0f);
+	float idetity[] = {
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1
+	};
 
 	MSG msg;
 	while (true)
@@ -74,8 +128,22 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 			DispatchMessage(&msg);
 		}
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(program);
 
+		glUniformMatrix4fv(MLocation, 1, GL_FALSE, idetity);
+		glUniformMatrix4fv(VLocation, 1, GL_FALSE, idetity);
+		glUniformMatrix4fv(PLocation, 1, GL_FALSE, glm::value_ptr(projectionMat));
 
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(posLocation);
+		glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)NULL);
+
+		glEnableVertexAttribArray(colorLocation);
+		glVertexAttribPointer(colorLocation, 4, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)(sizeof(float)*3));
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glUseProgram(0);
 		SwapBuffers(dc);
 	}
 	return 0;
